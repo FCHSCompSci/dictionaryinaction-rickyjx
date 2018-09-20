31c40aa6e38519d242ebf3b4f5f0f6715f5f0565
Bianca Janee Rush rox <bianca.rushrox@stu.jefferson.kyschools.us>
2018-09-20 08:32:58 -0400

    commit issues 2

---
+++ street fight.py
@@ -0,0 +1,36 @@
+print ("Your fighter is being processed...")
+
+import time
+time.sleep (2)
+
+your_fighter = {
+    'health': 100,
+    'speed' : 10,
+    'strenth' : 5,
+    }
+boss_fight = {
+    'health' : 500,
+    'speed' : 3,
+    'strenth' : 15,
+    }
+#narrator
+print ("Good luck fighter! press 8 to contiue")
+movement = [ 8, 4, 6, 2, ]
+if movement == 8:
+    print ("crash!")
+
+import time
+time.sleep (4)
+
+#boss
+print ("...Do I hear someone in my dounger!?")
+
+import time
+time.sleep (2)
+
+print ("prepare to die!")
+
+
+
+
+    
