8ae7ac130af635d4670b58bd7a1196ee786f9b54
Bianca Janee Rush rox <bianca.rushrox@stu.jefferson.kyschools.us>
2018-09-24 08:15:29 -0400

    commitment issues 2

--- street fight.py
+++ street fight.py
@@ -5,29 +5,32 @@ your_fighter = {
     'speed' : 10,
     'strenth' : 5,
     'curr_x' : 0,
-     'curr_' : 0, 
+    'curr_y' : 0, 
     }
 boss_fight = {
     'health' : 500,
     'speed' : 3,
     'strenth' : 15,
     }
-print ("p[ress 8 tp start")
+
 boss_moves = [ 50, 10, 30, 25]
-while your_fighter['health'] < 0:
+
+move = input ("how would you like to move?")
+
+while your_fighter['curr_y'] < 5:
     
-    move = input ("Which way would you like to move? [S]lash [q] to quite")
-    
-    if move == 8:
         your_fighter['curr_y'] = your_fighter['curr_y'] +1
-        print (random.choice(boss_moves))
-    elif move == 6:
-        your_fighter['curr_y'] = your_fighter['curr_y'] +1
+        print (your_fighter['curr_y'])
+          
+  
+        
+        your_fighter['curr_x'] = your_fighter['curr_x'] +1
         print (random.choice(boss_moves))
            
     elif move == "q":
         break
     elif move == "S":
-        boss_fight['health'] = boss_fight['health'] - Q
+        boss_fight['health'] = boss_fight['health'] - S
         print(boss_figth ['health'])
     
+  
